81e903602da379aff84f57f9eb6f3d4eadf0a129
rahulmahadik2 <31536219+rahulmahadik2@users.noreply.github.com>
2017-09-17 20:35:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+// counter value needs to be incremented
+
+var button = document.getElementById('counter');
+counter=0;
+button.onclick= function()
+{
+    // make a request
+    
+    
+    // capture the response
+    
+    // render the variable
+    counter=counter++;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+}
