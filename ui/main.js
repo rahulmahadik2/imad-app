04e963a4af217760200a36e490d8bf0b45a75af5
rahulmahadik2 <31536219+rahulmahadik2@users.noreply.github.com>
2017-09-17 21:00:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,24 @@
 // counter value needs to be incremented
 
 var button = document.getElementById('counter');
-var counter=0;
 button.onclick= function(){
-    // make a request
+    // create a request object
     
-    
+    var request= new XMLHttpRequest();
     // capture the response
+    request.onreadystatechange = function(){
+      if (request.readystate === XMLHttpRequest.DONE){
+       if(request.status===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+           
+       }    
+      }
+    };
+    
     
     // render the variable
-    counter=counter + 1; 
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+        request.open('GET', 'https://coco98.imad.hasura-app.io/counter',true);
+        request.send(null);
 };
