cf74d4ab4a6ea9f0acb16dde7025cef1c3c57c30
rahulmahadik2 <31536219+rahulmahadik2@users.noreply.github.com>
2017-09-17 21:06:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,6 @@ button.onclick= function(){
             var counter = request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
-           
        }    
       }
     };
